f2643fdfe0b9bfa8586c0218bf1ff97fad455db7
wagnerAJS221 <wagneralex221@gmai.com>
2023-01-03 12:43:55 +0000

    core done

---
+++ src/server.js
@@ -0,0 +1,44 @@
+//Include the express library
+const express = require('express')
+//Include the morgan middleware
+const morgan = require('morgan')
+//Include the cors middleware
+const cors = require('cors')
+
+//Create a new express application
+const app = express()
+
+//Tell express we want to use the morgan library
+app.use(morgan('dev'))
+//Tell express we want to use the cors library
+app.use(cors())
+//Tell express to parse JSON in the request body
+app.use(express.json())
+
+let counter = 10
+
+app.get('/counter', (req, res) => {
+  res.json({ counter: counter })
+})
+
+app.post('/counter/increment', (req, res) => {
+  counter += 1
+  res.json({ counter: counter })
+})
+
+app.post('/counter/double', (req, res) => {
+  counter *= 2
+  res.json({ counter: counter })
+})
+
+app.post('/counter/decrement', (req, res) => {
+  counter -= 1
+  res.json({ counter: counter })
+})
+
+app.delete('/counter', (req, res) => {
+  counter = 0
+  res.json({ counter: counter })
+})
+
+module.exports = app
