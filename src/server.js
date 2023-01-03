d1867cd61710ce3b84e10ed48af25a8e0b54d9a5
Joel Watkins <eck087@yahoo.co.uk>
2023-01-03 15:10:39 +0000

    changed status codes to pass tests

--- src/server.js
+++ src/server.js
@@ -1,44 +1,49 @@
 //Include the express library
-const express = require('express')
+const express = require("express");
 //Include the morgan middleware
-const morgan = require('morgan')
+const morgan = require("morgan");
 //Include the cors middleware
-const cors = require('cors')
+const cors = require("cors");
 
 //Create a new express application
-const app = express()
+const app = express();
 
 //Tell express we want to use the morgan library
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 //Tell express we want to use the cors library
-app.use(cors())
+app.use(cors());
 //Tell express to parse JSON in the request body
-app.use(express.json())
+app.use(express.json());
 
-let counter = 10
+let counter = 0;
 
-app.get('/counter', (req, res) => {
-  res.json({ counter: counter })
-})
+app.get("/counter", (req, res) => {
+  res.json({ counter: counter });
+});
 
-app.post('/counter/increment', (req, res) => {
-  counter += 1
-  res.json({ counter: counter })
-})
+app.post("/counter/increment", (req, res) => {
+  counter += 1;
+  res.status(201).json({ counter: counter });
+});
 
-app.post('/counter/double', (req, res) => {
-  counter *= 2
-  res.json({ counter: counter })
-})
+app.post("/counter/double", (req, res) => {
+  counter *= 2;
+  res.status(201).json({ counter: counter });
+});
 
-app.post('/counter/decrement', (req, res) => {
-  counter -= 1
-  res.json({ counter: counter })
-})
+app.post("/counter/decrement", (req, res) => {
+  counter -= 1;
+  res.status(201).json({ counter: counter });
+});
 
-app.delete('/counter', (req, res) => {
-  counter = 0
-  res.json({ counter: counter })
-})
+app.delete("/counter", (req, res) => {
+  counter = 0;
+  res.json({ counter: counter });
+});
 
-module.exports = app
+app.put("/counter", (req, res) => {
+  counter = 0;
+  res.json({ counter: counter });
+});
+
+module.exports = app;
