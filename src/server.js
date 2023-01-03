f308b0ade07292cd954e50034d380fa1bc85c720
Dave <Daverevell491@gmail.com>
2023-01-03 17:21:15 +0000

    Ext.2 completed

--- src/server.js
+++ src/server.js
@@ -17,33 +17,73 @@ app.use(express.json());
 
 let counter = 0;
 
+let counters = {
+	spaceMarines: 1000,
+	orks: 10,
+	cars: 0,
+};
+
 app.get("/counter", (req, res) => {
-  res.json({ counter: counter });
+	res.json({ counter: counter });
 });
 
 app.post("/counter/increment", (req, res) => {
-  counter += 1;
-  res.status(201).json({ counter: counter });
+	counter += 1;
+	res.status(201).json({ counter: counter });
 });
 
 app.post("/counter/double", (req, res) => {
-  counter *= 2;
-  res.status(201).json({ counter: counter });
+	counter *= 2;
+	res.status(201).json({ counter: counter });
 });
 
 app.post("/counter/decrement", (req, res) => {
-  counter -= 1;
-  res.status(201).json({ counter: counter });
+	counter -= 1;
+	res.status(201).json({ counter: counter });
 });
 
 app.delete("/counter", (req, res) => {
-  counter = 0;
-  res.json({ counter: counter });
+	counter = 0;
+	res.json({ counter: counter });
 });
 
+// Ext.1
+
 app.put("/counter", (req, res) => {
-  counter = req.query.value;
-  res.status(201).res.json({ counter: counter });
+	counter = req.query.value;
+	res.status(201).json({ counter: counter });
+});
+
+// Ext.2
+
+app.get("/counter/:name", (req, res) => {
+	const counterValue = counters[req.params.name];
+	res.json({ counter: counterValue });
+});
+
+app.post("/counter/:name/increment", (req, res) => {
+	counters[req.params.name] += 1;
+	res.status(201).json({ counter: counters[req.params.name] });
+});
+
+app.post("/counter/:name/decrement", (req, res) => {
+	counters[req.params.name] -= 1;
+	res.status(201).json({ counter: counters[req.params.name] });
+});
+
+app.post("/counter/:name/double", (req, res) => {
+	counters[req.params.name] *= 2;
+	res.status(201).json({ counter: counters[req.params.name] });
+});
+
+app.delete("/counter/:name", (req, res) => {
+	counters[req.params.name] = 0;
+	res.json({ counter: counters[req.params.name] });
+});
+
+app.put("/counter/:name", (req, res) => {
+	counters[req.params.name] = Number(req.query.value);
+	res.status(201).json({ counter: counters[req.params.name] });
 });
 
 module.exports = app;
