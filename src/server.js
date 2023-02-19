d17738a1af35a488b9cf25fdf4b0c3b201ba9747
akibria12 <ahkibria@hotmail.co.uk>
2023-02-19 13:57:50 +0000

    exercise done

--- src/server.js
+++ src/server.js
@@ -45,10 +45,45 @@ app.post("/counter/double", (req, res) => {
   res.status(201).json({ counter });
 });
 
-// Ext 1
+// Ext 1(Set the counter to a specific value via a query parameter)
 app.put("/counter", (req, res) => {
   counter = Number(req.query.value);
   res.status(201).json({ counter });
+});
+
+// Ext 2
+// GET(Retrieve the current counter for the provided counter name)
+let counterList = {
+  cars: 0,
+  planes: 50,
+  rockets: 100,
+};
+app.get("/counter/:name", (req, res) => {
+  res.json({ counter: counterList[req.params.name] });
+});
+
+// DELETE(Reset the counter for the provided name to 0)
+app.delete("/counter/:name", (req, res) => {
+  counterList[req.params.name] = 0;
+  res.json({ counter: counterList[req.params.name] });
+});
+
+// Increment(Increment the counter for the provided name)
+app.post("/counter/:name/increment", (req, res) => {
+  counterList[req.params.name] += 1;
+  res.status(201).json({ counter: counterList[req.params.name] });
+});
+
+// Decrement(Deccrement the counter for the provided name)
+app.post("/counter/:name/decrement", (req, res) => {
+  counterList[req.params.name] -= 1;
+  res.status(201).json({ counter: counterList[req.params.name] });
+});
+
+// Double(Double the counter for the provided name)
+app.post("/counter/:name/double", (req, res) => {
+  counterList[req.params.name] *= 2;
+  res.status(201).json({ counter: counterList[req.params.name] });
 });
 
 module.exports = app;
