144a8e2d7a1b7b957442f8ae062fd9fee553f501
Jenocke <jenocke@gmail.com>
2023-03-13 17:08:28 +0100

    exercice complete

--- src/server.js
+++ src/server.js
@@ -14,4 +14,30 @@ app.use(cors());
 //Tell express to parse JSON in the request body
 app.use(express.json());
 
+const counter = { counter: 0 };
+
+app.get("/counter", (req, res) => {
+  res.status(200).json({ counter: counter.counter });
+});
+
+app.delete("/counter", (req, res) => {
+  counter.counter = 0;
+  res.status(200).json({ counter: counter.counter });
+});
+
+app.post("/counter/increment", (req, res) => {
+  counter.counter++;
+  res.status(201).json({ counter: counter.counter });
+});
+
+app.post("/counter/decrement", (req, res) => {
+  counter.counter--;
+  res.status(201).json({ counter: counter.counter });
+});
+
+app.post("/counter/double", (req, res) => {
+  counter.counter = counter.counter * 2;
+  res.status(201).json({ counter: counter.counter });
+});
+
 module.exports = app;
