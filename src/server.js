3c665ce6a4ba9133f1e616a9a2cc0f20db39b215
Asiyeyurtkuran <asiyeyurtkuran@gmail.com>
2023-03-13 17:43:43 +0000

    extensions1 and 2 passed

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const express = require("express")
 const morgan = require("morgan")
 //Include the cors middleware
 const cors = require("cors")
+// const { query } = require("express")
 
 //Create a new express application
 const app = express()
@@ -20,12 +21,11 @@ let counter = 0
 //1. request query
 //2. request parameters
 
-// const counters = {
-//    { id: 1, value: 5},
-//    { id: 3, value:0 }
-// }
+const counters = [
+    { value: 0, name: "cars" }
+  ];
 
-app.get('/counter' , (req, res) => {
+app.get('/counter', (req, res) => {
     //1.
     // console.log("get in the/counter")
     // console.log(req.query.value)
@@ -46,7 +46,6 @@ app.post('/counter/increment', (req, res) => {
 // app.post('/counter/:id/increment', (req, res) => {
 //     const id = req.params.id // think about data type string or integer?
 //     //http://localhost:3030/counter/1/increment
-
 //     //find the right counter
 //     counter+=1
 //     //res.json({counter})
@@ -57,7 +56,7 @@ app.post('/counter/increment', (req, res) => {
 // delete - counter
 app.delete('/counter', (req, res) => {
     counter = 0
-    res.json({counter})
+    res.json({counter: counter})
 })
 
 // post - counter decrement
@@ -72,6 +71,66 @@ app.post('/counter/double', (req, res) => {
     res.status(201).json({counter})
 })
 
+//extension 1
+app.put('/counter', (req, res) => {
+    counter = Number(req.query.value)
+    res.status(201).json({counter: counter})
+})
+
+// extension 2
+app.get('/counter/:name', (req, res) => {
+    const name = req.params.name;
+     const found = counters.find((counter) => counter.name === name);
+    res.send({ counter: found.value });
+  });
+
+
+app.delete('/counter/:name', (req, res) => {
+    const name = req.params.name;
+    const found = counters.find((counter) => counter.name === name)
+    found.value = 0 ;
+    res.send({counter: found.value})
+})
+
+
+app.post('/counter/:name/increment', (req, res) => {
+    const name = req.params.name;
+    const found = counters.find((counter) => counter.name === name)
+    found.value++ ;
+    res.status(201).json({counter: found.value})
+})
+
+app.post('/counter/:name/decrement', (req, res) => {
+    const name = req.params.name;
+    const found = counters.find((counter) => counter.name === name)
+    found.value-- ;
+    res.status(201).json({counter: found.value})
+})
+
+app.put("/counter/:name", (req, res) => {
+    const name = req.params.name;
+    const value = req.query.value;
+    const found = counters.find((counter) => counter.name === name);
+    found.value = value;
+    res.status(201).json({ counter: found.value });
+  });
+  
+ 
+app.post('/counter/:name/double', (req, res) => {
+    const name = req.params.name;
+    const found = counters.find((counter) => counter.name === name)
+    found.value = found.value*2 ;
+    res.status(201).json({counter: found.value})
+})
 
 
 module.exports = app
+
+
+
+  
+
+  
+
+  
+ 
