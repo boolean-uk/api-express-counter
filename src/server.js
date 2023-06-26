b07fc5a872fda136074d1dbb84eb5b85b0d5dac4
Ian Grantham <dev.iangrantham@gmail.com>
2023-06-26 15:32:37 +0100

    get, delete, increment functionality added

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const express = require("express")
 const morgan = require("morgan")
 //Include the cors middleware
 const cors = require("cors")
+const req = require("express/lib/request")
 
 //Create a new express application
 const app = express()
@@ -14,5 +15,21 @@ app.use(morgan("dev"))
 app.use(cors())
 //Tell express to parse JSON in the request body
 app.use(express.json())
+
+let counter = 0
+
+app.get('/counter', (req, res) => {
+  return res.send({ counter })
+})
+
+app.delete('/counter', (req, res) => {
+  counter = 0 
+  return res.send({ counter })
+})
+
+app.post('/counter/increment', (req, res) => {
+  counter++
+  return res.send( { counter })
+})
 
 module.exports = app
