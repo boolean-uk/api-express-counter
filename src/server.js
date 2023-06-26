b1d289b8632707b74bdde1f463bacb3059a291e7
gid-ctrl <gideonusenbor@yahoo.co.uk>
2023-06-26 15:45:38 +0100

    core complete

--- src/server.js
+++ src/server.js
@@ -1,35 +1,45 @@
 //Include the express library
-const express = require("express")
+const express = require("express");
 //Include the morgan middleware
-const morgan = require("morgan")
+const morgan = require("morgan");
 //Include the cors middleware
-const cors = require("cors")
-const req = require("express/lib/request")
+const cors = require("cors");
+const req = require("express/lib/request");
 
 //Create a new express application
-const app = express()
+const app = express();
 
 //Tell express we want to use the morgan library
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 //Tell express we want to use the cors library
-app.use(cors())
+app.use(cors());
 //Tell express to parse JSON in the request body
-app.use(express.json())
+app.use(express.json());
 
-let counter = 0
+let counter = 1;
 
-app.get('/counter', (req, res) => {
-  return res.send({ counter })
-})
+app.get("/counter", (req, res) => {
+  return res.send({ counter });
+});
 
-app.delete('/counter', (req, res) => {
-  counter = 0 
-  return res.send({ counter })
-})
+app.delete("/counter", (req, res) => {
+  counter = 0;
+  return res.send({ counter });
+});
 
-app.post('/counter/increment', (req, res) => {
-  counter++
-  return res.send( { counter })
-})
+app.post("/counter/increment", (req, res) => {
+  counter++;
+  return res.send({ counter });
+});
 
-module.exports = app
+app.post("/counter/decrement", (req, res) => {
+  --counter;
+  return res.send({ counter });
+});
+
+app.post("/counter/double", (req, res) => {
+  counter = counter * 2;
+  return res.send({ counter });
+});
+
+module.exports = app;
