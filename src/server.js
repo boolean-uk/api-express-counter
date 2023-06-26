854533bafb1ac67a10dc1dddd849780fa876d5cd
Lee Kelly <leeowenkelly@gmail.com>
2023-06-26 16:57:42 +0100

    extensions completed

--- src/server.js
+++ src/server.js
@@ -1,51 +1,108 @@
 //Include the express library
-const express = require("express")
+const express = require("express");
 //Include the morgan middleware
-const morgan = require("morgan")
+const morgan = require("morgan");
 //Include the cors middleware
-const cors = require("cors")
+const cors = require("cors");
 
 //Create a new express application
-const app = express()
+const app = express();
 
 //Tell express we want to use the morgan library
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 //Tell express we want to use the cors library
-app.use(cors())
+app.use(cors());
 //Tell express to parse JSON in the request body
-app.use(express.json())
+app.use(express.json());
 
-const data = [
-    {"name": '', counter: 0}
-]
+const data = { counter: 0 };
+const dataArr = [
+  { name: "norik", counter: 0 },
+  { name: "tom", counter: 0 },
+  { name: "lee", counter: 0 },
+];
 
-app.get('/counter', (req, res) => {
-    return res.send(data)
+const findName = (name) => {
+  return dataArr.find((counter) => counter.name === name);
+};
+
+app.get("/counter", (req, res) => {
+  return res.send(data);
+});
+
+app.post("/counter/increment", (req, res) => {
+  data.counter += 1;
+  return res.status(201).send(data);
+});
+
+app.post("/counter/decrement", (req, res) => {
+  data.counter -= 1;
+  return res.status(201).send(data);
+});
+
+app.post("/counter/double", (req, res) => {
+  data.counter *= 2;
+  return res.status(201).send(data);
+});
+
+app.delete("/counter", (req, res) => {
+  data.counter = 0;
+  return res.status(200).send(data);
+});
+
+app.put("/counter", (req, res) => {
+  const num = req.query.counter;
+  data.counter = Number(num);
+  return res.status(201).send(data);
+});
+
+app.get("/counter/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const foundName = findName(name);
+
+  return typeof foundName !== "undefined"
+    ? res.status(200).send({ counter: foundName.counter })
+    : res.status(404).send("Not found");
+});
+
+app.delete("/counter/:name", (req, res) => {
+    const name = req.params.name.toLowerCase();
+    const findIndex = dataArr.findIndex((counter) => counter.name === name);
+
+    return findIndex !== -1
+    ? (dataArr[findIndex].counter = 0,
+      res.status(201).send({ counter: dataArr[findIndex].counter }))
+    : res.status(404).send("Not found");
 })
 
-app.post('/counter/increment', (req, res) => {
-    data.counter += 1
+app.post("/counter/:name/increment", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const findIndex = dataArr.findIndex((counter) => counter.name === name);
 
-    return res.status(201).send(data)
-})
-app.post('/counter/decrement', (req, res) => {
-    data.counter -= 1
+  return findIndex !== -1
+    ? (dataArr[findIndex].counter++,
+      res.status(201).send({ counter: dataArr[findIndex].counter }))
+    : res.status(404).send("Not found");
+});
 
-    return res.status(201).send(data)
-})
-app.post('/counter/double', (req, res) => {
-    data.counter *= 2
+app.post("/counter/:name/decrement", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const findIndex = dataArr.findIndex((counter) => counter.name === name);
 
-    return res.status(201).send(data)
-})
-app.delete('/counter', (req, res) => {
-    data.counter = 0
+  return findIndex !== -1
+    ? (dataArr[findIndex].counter--,
+      res.status(201).send({ counter: dataArr[findIndex].counter }))
+    : res.status(404).send("Not found");
+});
 
-    return res.status(200).send(data)
-})
-app.put('/counter', (req, res) => {
-    const num = req.query.value
-    data.counter = Number(num)
-    return res.status(201).send(data)
-})
-module.exports = app
+app.post("/counter/:name/double", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const findIndex = dataArr.findIndex((counter) => counter.name === name);
+
+  return findIndex !== -1
+    ? (dataArr[findIndex].counter*=2,
+      res.status(201).send({ counter: dataArr[findIndex].counter }))
+    : res.status(404).send("Not found");
+});
+
+module.exports = app;
