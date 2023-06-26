3fd545c604f726f621421c786109c612312d996a
carolina arruda <carolinacalouroarruda@gmail.com>
2023-06-26 17:17:32 +0200

    ext 1 done

--- src/server.js
+++ src/server.js
@@ -1,47 +1,51 @@
 //Include the express library
-const express = require("express")
+const express = require("express");
 //Include the morgan middleware
-const morgan = require("morgan")
+const morgan = require("morgan");
 //Include the cors middleware
-const cors = require("cors")
+const cors = require("cors");
 
 //Create a new express application
-const app = express()
+const app = express();
 
 //Tell express we want to use the morgan library
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 //Tell express we want to use the cors library
-app.use(cors())
+app.use(cors());
 //Tell express to parse JSON in the request body
-app.use(express.json())
+app.use(express.json());
 
-let counter = 0
-   
+let counter = 0;
+
 // Retrieve the current counter
 
-app.get('/counter', (req, res) => {
-    return res.send( { counter } )
-})
+app.get("/counter", (req, res) => {
+  return res.send({ counter });
+});
 
-app.delete('/counter', (req, res) => {
-    counter = 0 
-    return res.send({ counter: counter })
-})
+app.delete("/counter", (req, res) => {
+  counter = 0;
+  return res.send({ counter: counter });
+});
 
-app.post('/counter/increment', (req, res) => {
-    counter++
-    return res.status(201).send({ counter: counter })
-    
-})
-app.post('/counter/decrement', (req, res) => {
-    counter--
-    return res.status(201).send({ counter: counter })
-    
-})
+app.post("/counter/increment", (req, res) => {
+  counter++;
+  return res.status(201).send({ counter: counter });
+});
+app.post("/counter/decrement", (req, res) => {
+  counter--;
+  return res.status(201).send({ counter: counter });
+});
 
-app.post('/counter/double', (req, res) => {
-    counter = counter * 2 
-    return res.status(201).send({ counter: counter })
-})
+app.post("/counter/double", (req, res) => {
+  counter = counter * 2;
+  return res.status(201).send({ counter: counter });
+});
 
-module.exports = app
+app.put("/counter", (req, res) => {
+  const { value } = req.query;
+  counter = Number (value)
+
+  return res.status(201).send({ counter });
+});
+module.exports = app;
