9c4e079a0898209db03a3e4b22687eda38c4bba6
ilham saleh <ilham2saleh@gmail.com>
2024-01-03 22:41:25 +0000

    Extension 2 - Retrieve current counter for provided name and set the counter for the provided name Passed test successfully

--- src/server.js
+++ src/server.js
@@ -46,4 +46,31 @@ app.put(`/counter`, (req, res) => {
   res.status(201).json(state);
 });
 
+const counters = {};
+
+app.get("/counter/:name", (req, res) => {
+  const counterName = req.params.name;
+  if (!counters[counterName]) {
+    counters[counterName] = 0;
+  }
+
+  res.json({ counter: counters[counterName] });
+});
+
+app.put("/counter/:name/set", (req, res) => {
+  const counterName = req.params.name;
+  const newValue = parseInt(req.query.value);
+
+  if (!counters[counterName]) {
+    counters[counterName] = 0;
+  }
+
+  if (!isNaN(newValue)) {
+    counters[counterName] = newValue;
+    res.json({ counter: counters[counterName] });
+  } else {
+    res.status(400).json({ error: "Invalid value provided." });
+  }
+});
+
 module.exports = app;
