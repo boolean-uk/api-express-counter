5bd5c0e6dcca26a8eec2e80c484bcc19c99fbe9b
ilham saleh <ilham2saleh@gmail.com>
2024-01-03 14:35:49 +0000

    Create counter endpoint and test 1 passed successfully

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
-
 // Import library/framworks
-const express = require('express')
-const morgan = require('morgan')
-const cors = require("cors")
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 // Define express as 'app'
-const app = express()
+const app = express();
 
 // Using frameworks with express
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
-module.exports = app
+const state = {
+  counter: 0,
+};
+
+// Get request
+app.get("/counter", (req, res) => {
+  res.status(200).json(state);
+});
+
+app.post("/counter/increment", (req, res) => {
+  state.counter++;
+  res.status(201).json(state);
+});
+
+module.exports = app;
