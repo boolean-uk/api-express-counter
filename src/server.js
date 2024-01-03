74b403aece1d883d10f762d84b32c9fa3740bbd5
Lukas Dembicki <lad_777@hotmail.com>
2024-01-03 15:03:15 +0000

    ADD - POST: '/counter/double'.

--- src/server.js
+++ src/server.js
@@ -1,40 +1,40 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const state = require('./counter.js')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const state = require("./counter.js");
+const app = express();
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.status(200).json("Welcome to a counter app");
+});
 
-app.get('/', (req, res) => {
-    res.status(200).json('Welcome to a counter app')
-})
+app.get("/counter", (req, res) => {
+    return res.status(200).json(state);
+});
 
-app.get('/counter', (req, res) => {
-    res.status(200).json(state)
-})
+app.delete("/counter", (req, res) => {
+    state.counter = 0;
+    return res.status(201).json(state);
+});
 
-app.delete('/counter', (req, res) => {
-    state.counter = 0
-    res.status(201).json(state)
-})
+app.post("/counter/increment", (req, res) => {
+    state.counter++;
+    return res.status(201).json(state);
+});
 
-app.post('/counter/increment', (req, res) => {
-    state.counter++
-    res.status(201).json(state)
-})
+app.post("/counter/decrement", (req, res) => {
+    state.counter--;
+    if (state.counter < 0) state.counter = 0
+    return res.status(201).json(state);
+});
 
-app.post('/counter/decrement', (req, res) => {
-    state.counter--
-    if(state.counter < 0) return state.counter = 0
-    res.status(201).json(state)
-})
+app.post("/counter/double", (req, res) => {
+    state.counter = state.counter * 2;
+    return res.status(201).json(state);
+});
 
-
-
-
-
-module.exports = app
+module.exports = app;
