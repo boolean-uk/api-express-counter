ee3cb0248158ad4db15929c831a5f57a646c6589
callumhayden <callum.hayden@gmail.com>
2024-01-03 14:32:13 +0000

    first test passsing, im receiving the counter

---
+++ src/server.js
@@ -0,0 +1,31 @@
+//Include the express library
+const express = require("express");
+//Include the morgan middleware
+const morgan = require("morgan");
+//Include the cors middleware
+const cors = require("cors");
+
+//Create a new express application
+const app = express();
+//Tell express we want to use the morgan library
+app.use(morgan("dev"));
+//Tell express we want to use the cors library
+app.use(cors());
+//Tell express to parse JSON in the request body
+app.use(express.json());
+
+let counter = 0;
+
+// GET request to the localhost:3030/ "root" path
+app.get('/', (req, res) => {
+    res.status(201).json({ message: 'Welcome to the counter!!' })
+  })
+
+// Get request
+app.get("/counter", (req, res) => {
+    res.json({counter: counter})
+  });
+
+  app.post()
+
+module.exports = app;
