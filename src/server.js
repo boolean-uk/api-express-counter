533e06a53dd305810f28bb2e55b9b68413187ce1
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-03 16:04:43 +0000

    feat: add get counter

--- src/server.js
+++ src/server.js
@@ -2,8 +2,16 @@ const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 
+
 const app = express()
 
+let count = 5
+
 app.use(morgan("dev"), cors(), express.json())
+
+app.get('/counter', (req, res) => {
+    console.log(count)
+    return res.json({"counter": count})
+})
 
 module.exports = app
