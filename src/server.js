dd524ae5f1e0cceca6ef785e138966f0e9dabb53
Lukas Dembicki <lad_777@hotmail.com>
2024-01-03 13:19:28 +0000

    add - import for counter. add - '/counter' GET. Returns counter JSON

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
+const state = require('./counter.js')
 const app = express()
 
 app.use(morgan('dev'))
@@ -9,7 +10,11 @@ app.use(express.json())
 
 
 app.get('/', (req, res) => {
-    res.json('Hello World')
+    res.json('Welcome to a counter app')
+})
+
+app.get('/counter', (req, res) => {
+    res.json(state)
 })
 
 module.exports = app
