15d276ca7c77967777c46a2143b270c557f8202b
Faiza <faraz_ul_islam@yahoo.com>
2024-01-03 23:19:53 +0000

    get counter

--- src/server.js
+++ src/server.js
@@ -15,9 +15,22 @@ app.use(cors())
 //Tell express to parse JSON in the request body
 app.use(express.json())
 
+let count = 0
 
-app.get('/',(req,res) => {
-res.json('hello world')
+app.get('/', (req , res) => {
+res.status(201).json({message:'counter API'})
 })
+
+/*app.get('/counter' , (req , res) => {
+    console.log(count)
+    return res.status(201).json({'counter': count})
+})*/
+app.get("/counter", (req, res) => {
+    const counter = {
+      counter: count,
+    };
+  
+    res.status(200).json(counter);
+  });
 
 module.exports = app
