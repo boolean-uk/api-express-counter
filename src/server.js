2ca75a7e2057b5438dcb82cdde17960f21b2af86
Lukas Dembicki <lad_777@hotmail.com>
2024-01-03 14:54:07 +0000

    ADD - PUT request, decrements counter.

--- src/server.js
+++ src/server.js
@@ -10,21 +10,26 @@ app.use(express.json())
 
 
 app.get('/', (req, res) => {
-    res.json('Welcome to a counter app')
+    res.status(200).json('Welcome to a counter app')
 })
 
 app.get('/counter', (req, res) => {
-    res.json(state)
+    res.status(200).json(state)
 })
 
 app.delete('/counter', (req, res) => {
     state.counter = 0
-    res.json(state)
+    res.status(201).json(state)
 })
 
 app.post('/counter/increment', (req, res) => {
     state.counter++
-    res.json(state)
+    res.status(201).json(state)
+})
+
+app.post('/counter/decrement', (req, res) => {
+    state.counter--
+    res.status(201).json(state)
 })
 
 
