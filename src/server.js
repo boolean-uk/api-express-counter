f7907f7c091ff98974e9291b47697d57ed649b90
Kye <NovusAa12@proton.me>
2024-01-03 15:13:55 +0000

    add counter api routes

--- src/server.js
+++ src/server.js
@@ -15,4 +15,51 @@ app.use(cors());
 //Tell express to parse JSON in the request body
 app.use(express.json());
 
+let count = 0;
+
+app.get("/", (req, res) => {
+  res.status(201).json({ message: "Counter API" });
+});
+
+app.get("/counter", (req, res) => {
+  const counter = {
+    counter: count,
+  };
+
+  res.status(200).json(counter);
+});
+
+app.delete("/counter", (req, res) => {
+  count = 0;
+  const counter = {
+    counter: count,
+  };
+
+  res.status(200).json(counter);
+});
+
+app.post("/counter/increment", (req, res) => {
+  const counter = {
+    counter: ++count,
+  };
+
+  res.status(201).json(counter);
+});
+
+app.post("/counter/decrement", (req, res) => {
+  const counter = {
+    counter: --count,
+  };
+
+  res.status(201).json(counter);
+});
+
+app.post("/counter/double", (req, res) => {
+  const counter = {
+    counter: (count *= 2),
+  };
+
+  res.status(201).json(counter);
+});
+
 module.exports = app;
