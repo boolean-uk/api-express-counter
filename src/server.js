94100c108014505c0f71986d1c20c987401aefc4
radio58 <rt58gm@gmail.com>
2024-01-04 15:22:08 +0000

    fixed responses

--- src/server.js
+++ src/server.js
@@ -11,27 +11,27 @@ app.use(express.json())
 let counter = 0
 
 app.get('/counter', (req, res) => {
-    res.status(200).json(counter)
+    res.status(200).json({counter})
 })
 
 app.post('/counter/increment', (req, res) => {
     counter++
-    return res.status(201).json(counter)
+    return res.status(201).json({counter})
 })
 
 app.post('/counter/decrement', (req, res) => {
     counter--
-    return res.status(201).json(counter)
+    return res.status(201).json({counter})
 })
 
 app.post('/counter/double', (req, res) => {
     counter *= 2
-    return res.status(201).json(counter)
+    return res.status(201).json({counter})
 })
 
-app.post('/counter/reset', (req, res) => {
+app.delete('/counter', (req, res) => {
     counter = 0
-    return res.status(201).json(counter)
+    return res.status(200).json({counter})
 })
 
 module.exports = app
