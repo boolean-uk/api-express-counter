af88d491252b5b900b23b0d110d1daa7eee94c86
radio58 <rt58gm@gmail.com>
2024-01-04 14:40:10 +0000

    changed to status

--- src/server.js
+++ src/server.js
@@ -10,9 +10,8 @@ app.use(express.json())
 
 let counter = 0
 
-app.get('/', (req, res) => {
-    res.json(counter)
+app.get('/counter', (req, res) => {
+    res.status(200).json(counter)
 })
-
 
 module.exports = app
