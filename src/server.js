f0f73a7bf9d5a0d75dcec320fd5d584961fa0f15
aayushlama4008 <aayushlama4008@gmail.com>
2024-01-07 16:10:39 +0100

    create get request

--- src/server.js
+++ src/server.js
@@ -5,11 +5,21 @@ const morgan = require("morgan");
 //include the cors middleware
 const cors = require("cors");
 
-//create a new express application 
-const app = express()
+//create a new express application
+const app = express();
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
-module.exports = app
+const state = {
+  'counter': 0,
+};
+
+app.get("/counter", (req, res) => {
+  res.status(200).json(state);
+});
+
+
+
+module.exports = app;
