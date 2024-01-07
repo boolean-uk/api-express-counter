7e9301e6f917c82f219615c094b30abec4c3467b
aayushlama4008 <aayushlama4008@gmail.com>
2024-01-07 17:55:12 +0100

    added post double req to increase counter by double

--- src/server.js
+++ src/server.js
@@ -34,4 +34,9 @@ app.delete('/counter',(req,res)=>{
     state.counter = 0 
     return res.status(200).json(state)
 })
+
+app.post('/counter/double', (req,res)=>{
+    state.counter *= 2
+    return res.status(201).json(state)
+})
 module.exports = app;
