4a1f374181cb8ffa50b9ae4634efb63b7ef115c5
tomlukrprogram <tomlukeprogramming@outlook.com>
2024-01-08 22:35:01 +0000

    up to 9 done on readme

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const app = require('./server.js')
+const port = 3030
+
+app.listen(port, () => {
+ console.log(`Server is running on http://localhost:${port}/`)
+})
+
+//Include the express library
+const express = require("express")
+//Include the morgan middleware
+const morgan = require("morgan")
+//Include the cors middleware
+const cors = require("cors")
+
+//Create a new express application
+const app = express()
+
+//Tell express we want to use the morgan library
+app.use(morgan("dev"))
+//Tell express we want to use the cors library
+app.use(cors())
+//Tell express to parse JSON in the request body
+app.use(express.json())
+
+module.exports = app
