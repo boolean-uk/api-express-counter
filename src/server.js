e61cbf71bf1c877e6d7c21761d3b0a78b4519a7d
tomlukrprogram <tomlukeprogramming@outlook.com>
2024-01-08 23:29:52 +0000

    more tests passed

--- src/server.js
+++ src/server.js
@@ -11,17 +11,33 @@ app.use(express.json());
 let counter = 0;
 
 app.get("/counter", (req, res) => {
-  res.json({ count: counter });
+  res.status(200).json({ counter });
 });
 
 app.post("/counter/increment", (req, res) => {
   counter++;
-  res.json({ message: "Counter incremented", count: counter });
+  res.status(201).json({ message: "Counter incremented", counter });
 });
 
 app.post("/counter/decrement", (req, res) => {
   counter--;
-  res.json({ message: "Counter decremented", count: counter });
+  res.status(201).json({ message: "Counter decremented", counter });
+});
+
+app.post("/counter/double", (req, res) => {
+  counter *= 2;
+  res.status(201).json({ message: "Counter doubled", counter });
+});
+
+app.delete("/counter", (req, res) => {
+  counter = 0;
+  res.status(200).json({ message: "Counter reset", counter });
+});
+
+app.put("/counter", (req, res) => {
+  const { value } = req.query;
+  counter = parseInt(value);
+  res.status(201).json({ message: `Counter set to ${value}`, counter });
 });
 
 module.exports = app;
