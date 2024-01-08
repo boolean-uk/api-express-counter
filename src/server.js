1022d830904bdd3970dc67a2965cbf468bf341ee
tomlukrprogram <tomlukeprogramming@outlook.com>
2024-01-08 23:39:11 +0000

    one more test to pass

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 let counter = 0;
+let carsCounter = 0; 
 
 app.get("/counter", (req, res) => {
   res.status(200).json({ counter });
@@ -38,6 +39,30 @@ app.put("/counter", (req, res) => {
   const { value } = req.query;
   counter = parseInt(value);
   res.status(201).json({ message: `Counter set to ${value}`, counter });
+});
+
+app.get("/counter/cars", (req, res) => {
+  res.status(200).json({ counter: carsCounter });
+});
+
+app.post("/counter/cars/increment", (req, res) => {
+  carsCounter++;
+  res.status(201).json({ message: "Cars Counter incremented", counter: carsCounter });
+});
+
+app.post("/counter/cars/decrement", (req, res) => {
+  carsCounter--;
+  res.status(201).json({ message: "Cars Counter decremented", counter: carsCounter });
+});
+
+app.post("/counter/cars/double", (req, res) => {
+  carsCounter *= 2; 
+  res.status(201).json({ message: "Cars Counter doubled", counter: carsCounter });
+});
+
+app.delete("/counter/cars", (req, res) => {
+  carsCounter = 0;
+  res.status(200).json({ message: "Cars Counter reset", counter: carsCounter });
 });
 
 module.exports = app;
