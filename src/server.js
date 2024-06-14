b259d85c461079f3f7689b11c70ab7a164e0cac1
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-06-14 13:05:53 +0200

    add basic functionality

--- src/server.js
+++ src/server.js
@@ -8,4 +8,32 @@ app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 
+const counter = {
+    counter: 0
+}
+
+app.get('/counter', (req, res) => {
+    res.status(200).json(counter)
+})
+
+app.delete('/counter', (req, res) => {
+    counter.counter = 0
+    res.status(200).json(counter)
+})
+
+app.post('/counter/increment', (req, res) => {
+    counter.counter++
+    res.status(201).json(counter)
+})
+
+app.post('/counter/decrement', (req, res) => {
+    counter.counter--
+    res.status(201).json(counter)
+})
+
+app.post('/counter/double', (req, res) => {
+    counter.counter *= 2
+    res.status(201).json(counter)
+})
+
 module.exports = app
