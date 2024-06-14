196c45bfe20116d645ba6b37dbf1da172ae176a7
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-06-14 14:16:34 +0200

    add functionality for setting counter by name

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ app.use(express.json())
 const counter = {
     counter :0
 }
+
 const counterData = new Map()
 counterData.set('counter', counter)
 
@@ -21,31 +22,37 @@ app.get('/counter', (req, res) => {
 app.delete('/counter', (req, res) => {
     counter.counter = 0
     counterData.set('counter', counter)
+    
     res.status(200).json(counterData.get('counter'))
 })
 
 app.post('/counter/increment', (req, res) => {
     counter.counter++
     counterData.set('counter', counter)
+
     res.status(201).json(counterData.get('counter'))
 })
 
 app.post('/counter/decrement', (req, res) => {
     counter.counter--
     counterData.set('counter', counter)
+
     res.status(201).json(counterData.get('counter'))
 })
 
 app.post('/counter/double', (req, res) => {
     counter.counter *= 2
     counterData.set('counter', counter)
+
     res.status(201).json(counterData.get('counter'))
 })
 
 app.put('/counter', (req, res) => {
     const number = Number(req.query.value)
+
     counter.counter = number
     counterData.set('counter', counter)
+
     res.status(201).json(counterData.get('counter'))
 })
 
@@ -58,6 +65,52 @@ app.get('/counter/:name', (req, res) => {
     }
 
     res.status(200).json(counterData.get(name))
+})
+
+app.delete('/counter/:name', (req, res) => {
+    const name = req.params.name
+
+    counter.counter = 0
+    counterData.set(name, counter)
+
+    res.status(200).json(counterData.get(name))
+})
+
+app.post('/counter/:name/increment', (req, res) => {
+    const name = req.params.name
+
+    counter.counter++
+    counterData.set(name, counter)
+
+    res.status(201).json(counterData.get(name))
+})
+
+app.post('/counter/:name/decrement', (req, res) => {
+    const name = req.params.name
+
+    counter.counter--
+    counterData.set(name, counter)
+
+    res.status(201).json(counterData.get(name))
+})
+
+app.post('/counter/:name/double', (req, res) => {
+    const name = req.params.name
+
+    counter.counter *= 2
+    counterData.set(name, counter)
+
+    res.status(201).json(counterData.get(name))
+})
+
+app.put('/counter/:name', (req, res) => {
+    const name = req.params.name
+    const number = Number(req.query.value)
+
+    counter.counter = number
+    counterData.set(name, counter)
+
+    res.status(201).json(counterData.get(name))
 })
 
 
