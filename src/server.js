e36e8114c1719b763936706adefa8204837c65b5
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-06-14 14:06:42 +0200

    get counter by name

--- src/server.js
+++ src/server.js
@@ -8,43 +8,57 @@ app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 
-const qs = require('qs')
-app.set('query parser', function (str) {
-  return qs.parse(str, { /* custom options */ })
-})
-
 const counter = {
-    counter: 0
+    counter :0
 }
+const counterData = new Map()
+counterData.set('counter', counter)
 
 app.get('/counter', (req, res) => {
-    res.status(200).json(counter)
+    res.status(200).json(counterData.get('counter'))
 })
 
 app.delete('/counter', (req, res) => {
     counter.counter = 0
-    res.status(200).json(counter)
+    counterData.set('counter', counter)
+    res.status(200).json(counterData.get('counter'))
 })
 
 app.post('/counter/increment', (req, res) => {
     counter.counter++
-    res.status(201).json(counter)
+    counterData.set('counter', counter)
+    res.status(201).json(counterData.get('counter'))
 })
 
 app.post('/counter/decrement', (req, res) => {
     counter.counter--
-    res.status(201).json(counter)
+    counterData.set('counter', counter)
+    res.status(201).json(counterData.get('counter'))
 })
 
 app.post('/counter/double', (req, res) => {
     counter.counter *= 2
-    res.status(201).json(counter)
+    counterData.set('counter', counter)
+    res.status(201).json(counterData.get('counter'))
 })
 
 app.put('/counter', (req, res) => {
     const number = Number(req.query.value)
     counter.counter = number
-    res.status(201).json(counter)
+    counterData.set('counter', counter)
+    res.status(201).json(counterData.get('counter'))
 })
+
+app.get('/counter/:name', (req, res) => {
+    const name = req.params.name
+    const foundCounter = counterData.get(name)
+
+    if(!foundCounter) {
+        counterData.set(name, counter)
+    }
+
+    res.status(200).json(counterData.get(name))
+})
+
 
 module.exports = app
