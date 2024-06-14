a22d944bc4030349e2e02e7a7f7063a6c3f3b234
Will Baxter <bax.will@googlemail.com>
2024-06-14 12:02:26 +0100

    Get counter test passed

--- src/server.js
+++ src/server.js
@@ -1,19 +1,20 @@
-//Include the express library
-const express = require("express")
-//Include the morgan middleware
-const morgan = require("morgan")
-//Include the cors middleware
-const cors = require("cors")
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-//Create a new express application
-const app = express()
+const app = express();
 
-//Tell express we want to use the morgan library
-app.use(morgan("dev"))
-//Tell express we want to use the cors library
-app.use(cors())
-//Tell express to parse JSON in the request body
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
-//Export our app so other files can run it
-module.exports = app
+let counter = 0
+
+app.get('/counter', (req, res) => {
+    res.status(200).json({counter: counter})
+})
+
+
+
+
+module.exports = app;
