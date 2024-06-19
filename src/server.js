033b019dc6e79fa8c8beae8fa47efec9ad1697e4
Oluwagbemi Abiodun <oluwagbemiiabiodun@gmail.com>
2024-06-19 16:20:16 +0100

    xyz

--- src/server.js
+++ src/server.js
@@ -19,9 +19,18 @@ app.post('/counter/increment', (request, response) => {
 
 app.post('/counter/decrement', (request, response) => {
     myCounters[0].counter -= 1
-    response.json(myCounters)
+    response.status(201).json(myCounters)
 })
 
-app.post('/counter/de')
+app.post('/counter/double', (request, response) => {
+    myCounters[0].counter ++ 
+    response.status(201).json(myCounters)
+
+})
+
+app.delete('/counter/reset', (request, response) => {
+    myCounters[0].counter = 0
+    response.json(myCounters)
+})
 
 module.exports = app
